a20da3cafbef98fc222c7e0a58b91797f36cb993
Janette Rocha <113930409+jrocha26@users.noreply.github.com>
2022-10-04 17:25:35 +0000

    practiced for loops and while loops

--- main.py
+++ main.py
@@ -7,11 +7,28 @@
 # Random in python
 # List comprehension
 #importing modules
+#moduralizing your code
 #work on creating and understanding functions
 
-from decision_making import decisions
-from comparison_operators import comparison
-# from dictionaries_loops import dictionaries_loops
+# from decision_making import decisions
+# from comparison_operators import comparison
+# # from dictionaries_loops import dictionaries_loops
+from loops import loop
+# from enumerators import enumerators
+# from list_comprehension import list
+# from logical_operators import logical
+# from min_max import min_max
+# from random import random
+# from ranges import range
+# from while_loops import while_loops
 
-decisions()
-comparison()
+# decisions()
+# comparison()
+loop()
+# enumerators()
+# list()
+# logical()
+# min_max()
+# random()
+# range()
+# while_loops()
